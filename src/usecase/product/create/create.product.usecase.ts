e42c7659ab4ed996bea3a8924b9d7e6bbc5e52c8
André Soares Carvalho <andresoarescarvalho@gmail.com>
2024-04-04 17:01:00 -0300

    API's

--- src/usecase/product/create/create.product.usecase.ts
+++ src/usecase/product/create/create.product.usecase.ts
@@ -1,6 +1,3 @@
-import CustomerFactory from "../../../domain/customer/factory/customer.factory";
-import CustomerRepositoryInterface from "../../../domain/customer/repository/customer.repository.interface";
-import Address from "../../../domain/customer/value-object/address";
 import ProductFactory from "../../../domain/product/factory/product.factory";
 import ProductRepositoryInterface from "../../../domain/product/repository/product.repository.interface";
 import { InputCreateProductDto, OutputCreateProductDto } from "./create.product.dto";
